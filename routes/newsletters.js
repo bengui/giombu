2881e126a23d9c0c7fa786625fa9ecd772e9f176
Nicolas Ronchi <narc88@gmail.com>
2014-06-15 23:29:53 -0300

    Subscription fix y envio a suscriptores de la newsletter

--- routes/newsletters.js
+++ routes/newsletters.js
@@ -10,40 +10,46 @@ var mailer = require('express-mailer');
 module.exports = function (app){
 	//
 	app.get('/newsletters/sendTest/:id', function(req, res, next){
-		FranchiseModel.find({ _id : req.params.id}).exec( function(err, franchise){
-			if(err) throw err;
-			if(franchise){
-			  	DealModel.find( /*{"franchises": {$in: [req.body.id]}}*/ ).sort("-created").populate('franchises').populate("images").exec(function(err, deals){
-					if(err) throw err;
-					if(deals.length > 0){
-						for (var i = deals.length - 1; i >= 0; i--) {
-							deals_newsletter.push(deals[i]._id);
-						};
-						app.mailer.send('newsletters/default_template', {
-					  		to: 'narc88@gmail.com',
-					  		subject: 'Newsletter '+franchise.name, 
-					  		deals: deals,
-					  		franchise: franchise,
-					  		host: "http://localhost:3000"
-							}, function (err) {
-						    	if (err) {
-							      	console.log(err);
-							      	res.send('There was an error sending the email');
-							      	return;
-							    }
-							    newsletter = new NewsletterModel();
-							    newsletter.franchise = 
-							    newsletter.deals = deals_newsletter;
-							    newsletter.title = 'Newsletter '+franchise.name;
-							    newsletter.description = 'Newsletter para la franquicia'+ franchise.name;
-							    newsletter.save(function(){
-							    	res.redirect('/newsletters/'+newsletter.id)
-							    });
-							    
-							});
-					}
-			  	});
-			}
+		SubscriberModel.find({ "franchise" : req.params.id}).exec( function(err, subscribers){
+			FranchiseModel.find({ _id : req.params.id}).exec( function(err, franchise){
+				if(err) throw err;
+				if(franchise){
+				  	DealModel.find( /*{"franchises": {$in: [req.body.id]}}*/ ).sort("-created").populate('franchises').populate("images").exec(function(err, deals){
+						if(err) throw err;
+						if(deals.length > 0){
+							email_recievers = [];
+							for (var i = subscribers.length - 1; i >= 0; i--) {
+								email_recievers.push(subscribers[i].email)
+							};
+							for (var i = deals.length - 1; i >= 0; i--) {
+								deals_newsletter.push(deals[i]._id);
+							};
+							app.mailer.send('newsletters/default_template', {
+						  		to: email_recievers,
+						  		subject: 'Newsletter '+franchise.name, 
+						  		deals: deals,
+						  		franchise: franchise,
+						  		host: "http://localhost:3000"
+								}, function (err) {
+							    	if (err) {
+								      	console.log(err);
+								      	res.send('There was an error sending the email');
+								      	return;
+								    }
+								    newsletter = new NewsletterModel();
+								    newsletter.franchise = 
+								    newsletter.deals = deals_newsletter;
+								    newsletter.title = 'Newsletter '+franchise.name;
+								    newsletter.description = 'Newsletter para la franquicia'+ franchise.name;
+								    newsletter.save(function(){
+								    	res.redirect('/newsletters/'+newsletter.id)
+								    });
+								    
+								});
+						}
+				  	});
+				}
+			});
 		});
 	});
 										
