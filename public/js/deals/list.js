a748d52d944f4fbd070ddea7444db0dd9202298c
Maria de los Angeles Piuma <marupiuma@MacBook-de-Maria-de-los-Angeles-Piuma.local>
2014-09-01 08:28:04 -0300

    cambios

--- public/js/deals/list.js
+++ public/js/deals/list.js
@@ -8,6 +8,15 @@ $(document).ready(function(){
 			success 	: function(data){
 				$("#"+data.id+" .status_selector").removeClass("active draft closed");
 				$("#"+data.id+" .status_selector").addClass(data.status);
+				var status = "";
+				if(data.status === "draft"){
+					status = "Borrador";
+				}else if(data.status == "active") {
+					status = "Activa";
+				}else {
+					status = "Cerrada";
+				}	
+				$("#"+data.id+" .status_selector .status").html(status)
 			}
 		});
 	});
